218e31353f2b49ee527813383ea84cb514c7fb53
izaelgs <izael-123@outlook.com>
2024-12-17 10:55:14 -0300

    implement prisma and mongodb configuration

---
+++ pages/api/users.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+const userSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const parseResult = userSchema.safeParse(req.body);
+    if (!parseResult.success) {
+      return res.status(400).json({ errors: parseResult.error.errors });
+    }
+
+    const { name, email, password } = parseResult.data;
+
+    try {
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+
+      return res.status(201).json(user);
+    } catch (error) {
+      console.error("Erro ao criar usuário:" + error);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+
+  res.setHeader("Allow", ["POST"]);
+  res.status(405).end(`Method ${req.method} Not Allowed`);
+}
